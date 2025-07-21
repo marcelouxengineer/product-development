128db03a17820188b0d21a9b681fa108d4fd0478
marcelouxengineer <marcelo.uxengineer@gmail.com>
2025-07-21 20:45:45 -0300

    First attempt

---
+++ ux-to-dev-by-marcelo/src/content_option.js
@@ -0,0 +1,193 @@
+const logotext = "MARCELO";
+const meta = {
+    title: "Marcelo Antonietto",
+    description: "I’m Marcelo Antonietto _ Product Designer, Mobile devloper,currently working in Sao Paulo, Brazil",
+};
+
+const introdata = {
+    title: "I’m Marcelo Antonietto",
+    animated: {
+        first: "I solve problems with design",
+        second: "I develop mobile apps",
+        third: "I connect ideas to execution",
+    },
+    description: "Good design solves problems. Great design brings ideas to life - from the first sketch to the last line of code",
+    your_img_url: require("./assets/images/me.jpeg"),
+};
+
+const dataabout = {
+    title: "Overview",
+    aboutme: "In my pursuit of becoming a senior product designer, I strive to merge extensive experience with a user-centric approach. Alongside my ongoing studies in software engineering, I bring strategic thinking, clear communication, and adaptability to every challenge. I navigate problem-solving with attention to detail and empathy, ensuring that design choices align with both user needs and business goals. By bridging design and development, I aim to contribute meaningfully to product success and organizational growth.",
+};
+const worktimeline = [{
+        jobtitle: "Motorola Mobility - Product Designer",
+        where: "Sao Paulo, Brazil",
+        date: "2022- now",
+    },
+    {
+        jobtitle: "Bluma - Product Designer",
+        where: "Sao Paulo, Brazil",
+        date: "2022",
+    },
+    {
+        jobtitle: "Nullbug - Product Designer",
+        where: "Sao Paulo, Brazil",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Ux Research",
+        value: 95,
+    },
+    {
+        name: "UI design",
+        value: 85,
+    },
+
+    
+    {
+        name: "Prototyping (Figma",
+        value: 90,
+    },
+    {
+        name: "Interaction Design",
+        value: 90,
+    },
+    {
+        name: "Usability Testing",
+        value: 85,
+    },
+    {
+        name: "Design Systems",
+        value: 85,
+    },
+
+    {
+        name: "HTML/CSS",
+        value: 75,
+    },
+
+    {
+        name: "Git & Github",
+        value: 70,
+    },
+    
+
+        {
+            name: "Javascript",
+            value: 60,
+        },
+        {
+            name: "Python",
+            value: 50,
+        },
+     
+        
+    
+];
+
+
+
+const services = [{
+        title: "USP/ESALQ (2025-2027)",
+        description: " Software engineering MBA, Development, Engineering and AI",
+    },
+    {
+        title: "ESPM (2022-2024)",
+        description: "Master's degree, Strategic UX Design Management",
+    },
+    {
+        title: "FIAM-FAAM (2011-2016)",
+        description: "Bachelor's degree, Industrial and Product Design",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "marcelo.uxengineer@gmail.com",
+    YOUR_FONE: "(+55) 11 99696-2160",
+    description: "Whether you’re looking to collaborate, talk about product design, or discuss code, I’d love to hear from you. ",
+   YOUR_SERVICE_ID: "service_kg1ebqn",
+    YOUR_TEMPLATE_ID: "template_dpuvaqe",
+    YOUR_USER_ID: "X7APOxdw8N86s6wkH",
+};
+
+const socialprofils = {
+    github: "https://github.com/marcelouxengineer",
+       linkedin: "https://www.linkedin.com/in/marcelo-antonietto-4633b4207/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
